641e5254f13960bb92254edeee8e0618d8a96366
anakazato <aki.nakazato.office@gmail.com>
2025-12-18 02:19:16 +0900

    useHistoryを使ってJavascriptで画面遷移

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,11 +1,16 @@
-import {useLocation} from "react-router-dom";
+import {useLocation,useHistory} from "react-router-dom";
 
 export const Page1DetailA = () => {
   const {state} = useLocation();
   console.log(state);
+
+  const history = useHistory();
+  const onClickback = () => history.goBack();
+
   return (
       <div>
         <h1>Page1DetailA ページです</h1>
+        <button onClick={onClickback}>戻る</button>
       </div>
     );
   };
