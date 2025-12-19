649848b07f6c02502bd651e41c8d5ed3952c435b
anakazato <aki.nakazato.office@gmail.com>
2025-12-20 05:29:23 +0900

    CSS Modules で作ってみる

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
+import { CssModules } from "./components/CssModules";
 
 import { InlineStyle } from "./components/InlineStyle";
 
 export default function App() {
   return (
     <>
-    <InlineStyle></InlineStyle>
+    <InlineStyle />
+    <CssModules />
     </>
   );
 }
