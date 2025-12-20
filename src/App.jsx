881eec42054c99bae212514ad03ea6d8ebd77a83
anakazato <aki.nakazato.office@gmail.com>
2025-12-20 23:46:07 +0900

    Atom の作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import "./App.css";
+import { PrimaryButton } from "./components/atoms/buttons/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/buttons/SecondaryButton";
+
 export default function App() {
   return (
     <>
+    <div>
+    <PrimaryButton>テスト</PrimaryButton>
+    <SecondaryButton>検索</SecondaryButton>
+    </div>
     </>
   );
 }
