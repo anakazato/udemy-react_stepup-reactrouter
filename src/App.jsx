2afb6cba8b405334f9d5e4e92aa0756845609caa
anakazato <aki.nakazato.office@gmail.com>
2025-12-20 10:03:04 +0900

    Styled Jsx で作ってみる

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { CssModules } from "./components/CssModules";
 
 import { InlineStyle } from "./components/InlineStyle";
+import { StyledJsx } from "./components/StyledJsx";
 
 export default function App() {
   return (
     <>
     <InlineStyle />
     <CssModules />
+    <StyledJsx />
     </>
   );
 }
