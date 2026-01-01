75c7390fedfab8ef3b420038c5798ddb4310e823
anakazato <aki.nakazato.office@gmail.com>
2026-01-01 23:01:59 +0900

    Organism の作成

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,18 @@ import { PrimaryButton } from "./components/atoms/buttons/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/buttons/SecondaryButton";
 
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
+
+const user = {
+  name:"じゃけえ",
+  image:"https://images.unsplash.com/photo-1583512603805-3cc6b41f3edb?q=80&w=2360&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  email:"12345@aaa.com",
+  phone:"090-1234-5678",
+  company:{
+    name:"テスト株式会社",
+  },
+  website:"https://google.com",
+}
 
 export default function App() {
   return (
@@ -12,6 +24,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput></SearchInput>
+      <UserCard user={user} />
     </div>
     </>
   );
