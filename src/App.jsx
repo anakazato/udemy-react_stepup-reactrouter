5336b4a30de6a97947eb8d92f2c975ccf28ecf6c
anakazato <aki.nakazato.office@gmail.com>
2026-01-01 21:49:04 +0900

    Molecule の作成

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import "./App.css";
 import { PrimaryButton } from "./components/atoms/buttons/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/buttons/SecondaryButton";
 
+import { SearchInput } from "./components/molecules/SearchInput";
+
 export default function App() {
   return (
     <>
     <div>
-    <PrimaryButton>テスト</PrimaryButton>
-    <SecondaryButton>検索</SecondaryButton>
+      <PrimaryButton>テスト</PrimaryButton>
+      <SecondaryButton>検索</SecondaryButton>
+      <br />
+      <SearchInput></SearchInput>
     </div>
     </>
   );
